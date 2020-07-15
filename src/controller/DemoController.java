1360139282d25dd4c38562f16d9f33e5ff29be41
nvdinh185 <nvdinh0766777123@gmail.com>
2020-07-15 17:08:45 +0700

    add demo file

---
+++ src/controller/DemoController.java
@@ -0,0 +1,81 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/demo")
+public class DemoController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public DemoController() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+		// nạp driver
+		String username = "root";
+		String password = "";
+		String url = "jdbc:mysql://localhost:3306/tintuc?useUnicode=yes&characterEncoding=UTF-8";
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(url, username, password);
+			// hiển thị danh sách các danh mục
+			String sql = "SELECT * FROM danhmuctin";
+			st = conn.createStatement();
+			rs = st.executeQuery(sql);
+			while (rs.next()) {
+				int idDanhMuc = rs.getInt("id_danhmuctin");
+				String tenDanhMuc = rs.getString("tendanhmuctin");
+				out.println("ID danh muc: " + idDanhMuc + "<br />");
+				out.println("Tên danh muc: " + tenDanhMuc + "<br />");
+				out.println("----------------------------<br />");
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null)
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			if (st != null)
+				try {
+					st.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			if (conn != null)
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
