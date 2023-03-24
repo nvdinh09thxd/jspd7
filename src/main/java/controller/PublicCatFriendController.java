fc3bcc0c84a0da40ee5c14fea2e8cd59191c3546
nvdinh185 <nvdinh0766777123@gmail.com>
2023-03-24 16:48:27 +0700

    x

---
+++ src/main/java/controller/PublicCatFriendController.java
@@ -0,0 +1,42 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.Friend;
+import dao.FriendDao;
+
+public class PublicCatFriendController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public PublicCatFriendController() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int cid = 0;
+		try {
+			cid = Integer.parseInt(request.getParameter("cid"));
+		} catch (NumberFormatException e) {
+			RequestDispatcher rd = request.getRequestDispatcher("/BT3/PageNotFound.jsp");
+			rd.forward(request, response);
+			return;
+		}
+		ArrayList<Friend> listFriendsByIdCat = FriendDao.getItemsByIdCat(cid);
+		request.setAttribute("listFriendsByIdCat", listFriendsByIdCat);
+
+		RequestDispatcher rd = request.getRequestDispatcher("/BT3/danh-muc.jsp");
+		rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+}
---
+++ src/main/java/controller/PublicIndexFriendController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.Friend;
+import dao.FriendDao;
+
+public class PublicIndexFriendController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public PublicIndexFriendController() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		ArrayList<Friend> listFriends = FriendDao.getItems();
+		request.setAttribute("listFriends", listFriends);
+		RequestDispatcher rd = request.getRequestDispatcher("/BT3/index.jsp");
+		rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+}
